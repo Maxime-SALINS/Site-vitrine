2d76c74b4aed2ffbe283738ac9b174d27fbfdc24
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2023-12-10 22:56:47 +0100

    Update JS & visuel

---
+++ asset/js/services/contact-form.js
@@ -0,0 +1,21 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    // Important : Empêcher le comportement par défaut du navigateur
+    event.preventDefault();
+
+    // Récupération des valeurs des champs du formulaire dans des variables
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('message').value;
+
+    // Création d'un objet avec les valeurs des champs du formulaire
+    let infoContact = {
+        fname: fname,
+        lname: lname,
+        email: email,
+        subject : subject,
+        message: message
+    }
+    console.log(infoContact);
+});
